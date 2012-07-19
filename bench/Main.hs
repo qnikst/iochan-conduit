074ff3d8e2114f7db1835ba70d976aaf5543f120
Alexander Vershilov <alexander.vershilov@gmail.com>
2012-07-19 06:05:39 +0400

    benchmarks added

--- bench/Main.hs
+++ bench/Main.hs
@@ -1,19 +1,76 @@
-import Criterion.Main (default
+import Criterion
+import Criterion.Main (defaultMain)
+import Control.Monad
+import Control.Concurrent
+import Control.Concurrent.BMChan
+-- import Control.Concurrent.MChan
+import Control.Concurrent.STM
+import Data.Maybe
+import Data.Conduit
+import Data.Conduit.List as CL
+import Data.Conduit.Chan
+import Data.Conduit.TMChan
 
+-- | Run IO channel with concrete number of readers and writers
+-- finishes when all data is sent
 iochan nR nW n = do
-  ch <- newBMChan
-  lock <- newEmptyMVar
+  ch <- newBMChan 16
+  lock <- newQSemN 0
+  start_readers <- newEmptyMVar
   forkIO $ runResourceT $ sourceList [1..n] $$ sinkBMChan ch
+  forM_ [1..nR] $ 
+      const . forkIO $ do
+        _ <- readMVar start_readers -- start thread
+        runResourceT $ sourceBMChan ch $= CL.map f $$ consume
+        signalQSemN lock 1
+  putMVar start_readers ()
+  waitQSemN lock nR
+  return ()
+  where f = exp . log . exp . log . exp . log . exp . log . exp . log
+
+iochan2 nR nW n = do
+  ch <- newChan
+  lock <- newQSemN 0
+  start_readers <- newEmptyMVar
   forkIO $ do
-    runResourceT $ sourceBMChan ch $$ consume
-    putMVar lock ()
-  _ <- takeMVar
+    runResourceT $ sourceList [1..n] $= CL.map Just $$ sinkEndChan ch
+    writeChan ch Nothing
+  forM_ [1..nR] $ 
+      const . forkIO $ do
+        _ <- readMVar start_readers -- start thread
+        runResourceT $ sourceEndChan True ch $= CL.map (f . fromJust) $$ consume
+        signalQSemN lock 1
+  putMVar start_readers ()
+  waitQSemN lock nR
+  return ()
+  where f = exp . log . exp . log . exp . log . exp . log . exp . log
 
-
+stmchan nR nW n = do
+  ch <- atomically $ newTBMChan 16
+  lock <- newQSemN 0
+  start_readers <- newEmptyMVar
+  forkIO $ runResourceT $ sourceList [1..n] $$ sinkTBMChan ch
+  forM_ [1..nR] $ 
+      const . forkIO $ do
+        _ <- readMVar start_readers -- start thread
+        runResourceT $ sourceTBMChan ch $= CL.map f $$ consume
+        signalQSemN lock 1
+  putMVar start_readers ()
+  waitQSemN lock nR
+  return ()
+  where f = exp . log . exp . log . exp . log . exp . log . exp . log
 
 main = defaultMain [
-        bgroup "iochan" [ bench "10" $ nfIO iochan 10
-                        , bench "100" $ nfIO iochan 100
-                        , banch "1000" $ nfIO iochan 1000
+        bgroup "iochan" [ bench "1000"    $ nfIO $ iochan 1 1   10000
+                        , bench "1000:2"  $ nfIO $ iochan 1 2   10000
+                        , bench "1000:10" $ nfIO $ iochan 1 10  10000
+                        ]
+      , bgroup "iochan2"[ bench "1000"    $ nfIO $ iochan2 1 1   10000
+                        , bench "1000:2"  $ nfIO $ iochan2 1 2   10000
+                        , bench "1000:10" $ nfIO $ iochan2 1 10  10000
+                        ]
+      , bgroup "stm"    [ bench "1000"    $ nfIO $ stmchan 1 1  10000
+                        , bench "1000:2"  $ nfIO $ stmchan 1 2  10000
+                        , bench "1000:10" $ nfIO $ stmchan 1 10 10000
                         ]
       ]
